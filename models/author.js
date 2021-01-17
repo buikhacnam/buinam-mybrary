585bbb859bf3c0e693c23c11ff5c729f18ab09cf
Bui Nam <nambk@bravebits.vn>
2021-01-17 13:37:05 +0700

    fourth commit

--- models/author.js
+++ models/author.js
@@ -7,4 +7,14 @@ const authorSchema = new mongoose.Schema({
     }
 })
 
-module.exports = mongoose.model('AuthorName', authorSchema);
+module.exports = mongoose.model('Author', authorSchema);
+
+
+//meanwhile at mongo db:
+// _id: 6003d002e1c501001517ba7a
+// name: "nam"
+// __v: 0
+
+// _id: 6003d00de1c501001517ba7c
+// name: "bui"
+// __v: 0
--- routes/authors.js
+++ routes/authors.js
@@ -4,9 +4,18 @@ const Author = require('../models/author');
 
 // All authors Route
 router.get('/', async (req, res) => {
+    let searchOptions = {};
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions["name"] = new RegExp(req.query.name, 'i');
+       // searchOptions.name = req.query.name;
+    }
     try {
-        const authors = await Author.find({});
-        res.render('authors/index', { authors: authors});
+        const authors = await Author.find(searchOptions);
+        // ex: Author.find({'name': 'gau'})
+        res.render('authors/index', { 
+            authors: authors,
+            search: req.query.name,
+        });
     } catch {
         res.redirect('/');
     }
