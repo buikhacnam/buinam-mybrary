6ca04ddb7e8668f5c93e72a0250c35c0e818e0ed
Bui Nam <nambk@bravebits.vn>
2021-01-17 12:40:40 +0700

    third commit

--- routes/authors.js
+++ routes/authors.js
@@ -3,28 +3,38 @@ const router = express.Router();
 const Author = require('../models/author');
 
 // All authors Route
-router.get('/', (req, res) => {
-    res.render('authors/index');
+router.get('/', async (req, res) => {
+    try {
+        const authors = await Author.find({});
+        res.render('authors/index', { authors: authors});
+    } catch {
+        res.redirect('/');
+    }
+    
 })
 
 // New author Route
-router.get('/new', (req, res) => {
+router.get('/new',  (req, res) => {
+    
     res.render('authors/new', { author: new Author() });
 })
 
 // Create author Route
 router.post('/', async (req, res) => {
     const author = new Author({
-        name: req.body.name
+        name: req.body.authorName
     })
     try {
         const newAuthor = await author.save();
+        console.log('save new author')
         res.redirect('authors');
     } catch {
+        
         res.render('authors/new', {
             author: author,
             errorMessage: "Error creating Author"
         })
+        console.log(err)
     }
     // author.save((err, newAuthor) => {
     //     if(err) {
@@ -32,6 +42,7 @@ router.post('/', async (req, res) => {
     //             author: author,
     //             errorMessage: 'Error creating Author',
     //         })
+    //         console.log(err)
     //     } else {
     //         res.redirect(`authors`);
     //     }
